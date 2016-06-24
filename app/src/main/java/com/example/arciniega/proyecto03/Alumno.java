8a89697bb322d2f307757b0e34172fe657f860f3
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-06-23 18:00:59 -0600

    Clase alumno

---
+++ app/src/main/java/com/example/arciniega/proyecto03/Alumno.java
@@ -0,0 +1,58 @@
+package com.example.arciniega.proyecto03;
+
+/**
+ * Created by arciniega on 23/06/16.
+ */
+public class Alumno {
+    //Declaración de variables
+    private  int idAlumno;
+    private String matricula;
+    private String nombre;
+    private String aPaterno;
+    private String aMaterno;
+
+    //Constructor
+    public Alumno(){
+
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaPaterno() {
+        return aPaterno;
+    }
+
+    public void setaPaterno(String aPaterno) {
+        this.aPaterno = aPaterno;
+    }
+
+    public String getaMaterno() {
+        return aMaterno;
+    }
+
+    public void setaMaterno(String aMaterno) {
+        this.aMaterno = aMaterno;
+    }
+}
